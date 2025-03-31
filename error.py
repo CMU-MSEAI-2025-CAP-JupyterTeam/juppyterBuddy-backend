56d965aa49b20a051d074a4f6376bebc0cece15d
Wilson Ejulu <emox1995@gmail.com>
2025-03-31 03:57:07 +0200

    update _llm_node

---
+++ error.py
@@ -0,0 +1,8 @@
+{
+    "error": {
+        "message": "An assistant message with 'tool_calls' must be followed by tool messages responding to each 'tool_call_id'. The following tool_call_ids did not have response messages: call_qyKhjMmzfUKygwGOpx3bQoOG",
+        "type": "invalid_request_error",
+        "param": "messages",
+        "code": None,
+    }
+}
